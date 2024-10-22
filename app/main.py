6cfedd513cc0ed68b5bce6f7c0b5d8f116fd7b17
ivasslides <ivasslides@chonkster>
2024-10-22 10:40:15 -0400

    classwork for 10/22

--- app/main.py
+++ app/main.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
+#library to build a thing, have to import the package
 from fastapi import FastAPI
 from typing import Optional
+#pydantic allows us to create a model of the data
 from pydantic import BaseModel
+
 import json
 import os
 
+#instantiate the application (create an instance)
 app = FastAPI()
 
+#two endpoints for the api
+#using get method (pulling the data)
 @app.get("/")  # zone apex
+#function that takes no parameters
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Iliana"}
 
+#another get method
 @app.get("/add/{a}/{b}")
+#function called add takes two parameters of integers
 def add(a: int, b: int):
+#returns value of the sum of a + b
     return {"sum": a + b}
+
+#creating another endpoint
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
+
+
