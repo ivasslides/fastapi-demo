fc04049631bbe09b0c250c83a3d7e9e0ce4539f8
ivasslides <ivasslides@chonkster>
2024-11-14 00:24:30 -0500

    working on setting up connection string

--- app/main.py
+++ app/main.py
@@ -1,48 +1,15 @@
 #!/usr/bin/env python3
 
-#library to build a thing, have to import the package
-from fastapi import FastAPI
-from typing import Optional
-#pydantic allows us to create a model of the data
-from pydantic import BaseModel
-
-import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
-#instantiate the application (create an instance)
-app = FastAPI()
 
-#two endpoints for the api
-#using get method (pulling the data)
-@app.get("/")  # zone apex
-#function that takes no parameters
-def zone_apex():
-    return {"Hello": "Iliana"}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "fbv2sc"
 
-#another get method
-@app.get("/add/{a}/{b}")
-#function called add takes two parameters of integers
-def add(a: int, b: int):
-#returns value of the sum of a + b
-    return {"sum": a + b}
-
-#creating another endpoint
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
-
-#for lab 6, adding endpoint
-@app.get("/bday/{e}")
-def bday(e:str):
-    return{"birthday": e}
-
-#adding another
-@app.get("/hey")
-def hey(): 
-    return{"wassup": "shawty"}
-
-#adding one last one
-@app.get("/bye")
-def bye():
-    return{"goodbye": "see ya, adios, later gator"}
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
