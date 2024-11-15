ef4d262401bbcd142543d29cfc3eb743f8551ec8
ivasslides <ivasslides@chonkster>
2024-11-14 19:05:03 -0500

    finishing step 4

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import json
 import os
 import mysql.connector
 from mysql.connector import Error
@@ -12,4 +16,45 @@ DB = "fbv2sc"
 
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
+
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs(): 
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file AS file, songs.image AS image, songs.genre, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try: 
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e: 
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+
+
 
